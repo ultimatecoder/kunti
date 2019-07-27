f9045bb9503bd5285508afb837b15d71f19ce092
ultimatecoder <jaysinhp@gmail.com>
2019-07-28 00:37:46 +0530

    Adding functinality to assert List of posts works

--- kunti/blog/tests/test_comment.py
+++ kunti/blog/tests/test_comment.py
@@ -15,16 +15,23 @@ class TestComment(APITestCase):
     def _create_dummy_records(self):
         self.author = dummy.create_author()
         self.post = dummy.create_post(self.author)
-        self.comment = dummy.create_comment(self.author, self.post)
+        self.comments = [
+            dummy.create_comment(self.author, self.post),
+            dummy.create_comment(self.author, self.post)
+        ]
 
     def test_that_it_is_possible_to_read_a_comment(self):
         response = self.client.get('/comments/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         expected_data = {
-            "id": self.comment.id,
+            "id": self.comments[0].id,
             "post": self.post.id,
             "author": self.author.id,
-            "body": self.comment.body,
+            "body": self.comments[0].body,
             "created_date": mocks.MockedDateTime.timezoned
         }
         self.assertDictEqual(response.data, expected_data)
+
+    def test_that_it_is_possible_to_read_list_of_comments(self):
+        response = self.client.get('/comments/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
