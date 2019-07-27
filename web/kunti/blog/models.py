0578259fb11630fd19803d91b6a788121f5e9cb9
ultimatecoder <jaysinhp@gmail.com>
2019-07-27 18:19:45 +0530

    Adding functionality for storing Blog Post
    This block will add a functionality which will allow to store records
    generated for blog post from a User.

--- web/kunti/blog/models.py
+++ web/kunti/blog/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
+from django.contrib.auth import models as auth_models
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    author = models.ForeignKey(
+        auth_models.User,
+        on_delete=models.PROTECT
+    )
+    published_date = models.DateTimeField(auto_now=True)
+    title = models.TextField(max_length=100)
+    body = models.TextField(max_length=140, blank=True)
--- web/kunti/blog/tests.py
+++ web/kunti/blog/tests.py
@@ -1,11 +1,31 @@
+from django.contrib.auth import models as auth_models
 from django.test import TestCase
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from blog import models
 
-class TestBlog(APITestCase):
 
-    def test_that_it_is_possible_to_get_an_instance_of_blog(self):
-        response = self.client.get('blog/1')
+class TestPost(APITestCase):
+
+    def test_that_it_is_possible_to_get_an_instance_of_post(self):
+        abhilash = auth_models.User.objects.create(
+            username="abhilash",
+            password="12345",
+            email="abhilash@gmail.com",
+            first_name="Abhilash",
+            last_name="Sharma"
+        )
+        post = models.Post.objects.create(
+            author=abhilash,
+            title="Thanks Mahendra Singh Dhoni!",
+            body="I love Mahendra Singh Dhoni. He is great batsman."
+        )
+        response = self.client.get('post/1')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            "author": abhilash.id,
+            "title": post.title,
+            "body": post.body,
+        })
