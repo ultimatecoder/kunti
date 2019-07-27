49b5fc693f4723b865623132cab4a212454c8879
ultimatecoder <jaysinhp@gmail.com>
2019-07-27 22:34:28 +0530

    Possible to manage blog post from admin
    After this commit, it will be possible to manage blog posts from an
    admin interface.

--- kunti/blog/admin.py
+++ kunti/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog import models
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Post, PostAdmin)
